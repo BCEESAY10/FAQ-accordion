dd57fddd877cb428d38975b88aa4ecd433f17494
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-08 15:34:42 +0000

    Implement functionality

---
+++ index.js
@@ -0,0 +1,30 @@
+const accordionContent = document.querySelectorAll(".accordion-content");
+
+accordionContent.forEach((item) => {
+    let button = item.querySelector("button");
+
+    button.addEventListener('mouseenter', () => {
+        button.focus();
+    })
+
+    button.addEventListener('click', () => {
+        expand(item);
+    })
+})
+
+// Expan Answers
+const expand = (item) => {
+    let answer = item.querySelector('p');
+    let icon = item.querySelector('img');
+    let isOpen = false;
+    answer.classList.contains('hidden') ? isOpen=false : isOpen=true;
+
+    if(!isOpen){
+        answer.classList.remove('hidden');
+        icon.src.replace('assets/images/icon-plus.svg', 'assets/images/icon-minus.svg');
+    } else{
+        answer.classList.add('hidden');
+        icon.src.replace('assets/images/icon-minus.svg', 'assets/images/icon-plus.svg');
+    }
+
+}
